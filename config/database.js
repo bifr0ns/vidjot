fdae38dc475c6bf1d0a4a2e0cc80385dc8ac8a78
Hector Trejo <hate2411@gmail.com>
2018-03-08 18:32:05 -0600

    otra11

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV === 'production'){
-    module.exports = {mongoURI: 'mongodb://ale:ale@ds263138.mlab.com:63138/vidjot'}
+    module.exports = {mongoURI: 'mongodb://ale:@ds263138.mlab.com:63138/vidjot'}
   } else {
     module.exports = {mongoURI: 'mongodb://localhost/vidjot-dev'}
   }
